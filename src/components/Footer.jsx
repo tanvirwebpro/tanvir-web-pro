b8f00383afef5381da5893155c361fd791ee9b7c
tanvirwebpro <tanvirahmed9878@gmail.com>
2025-03-25 11:36:30 +0600

    sss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -109,7 +109,7 @@ const Footer = () => {
 
         <div className="flex items-center justify-between pt-10 mb-8">
           <a href="/" className="logo reveal-up">
-            <img src="https://i.ibb.co.com/8L8RFbr0/Tan-4.png" width={60} height={60} alt="Logo" />
+            <img src="https://i.ibb.co.com/8L8RFbr0/Tan-4.png" className="rounded-lg" width={60} height={60} alt="Logo" />
           </a>
 
           <p className="text-zinc-500 text-sm reveal-up">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,8 @@ const Header = () => {
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
         <h1>
           <a href="/" className="logo">
-            <img
+            <img 
+              className="rounded-lg"
               src="https://i.ibb.co.com/8L8RFbr0/Tan-4.png"
               width={60}
               height={60}
